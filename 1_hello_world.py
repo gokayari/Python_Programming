813bf1c2f6a2da5646a253a41038afe4b16d2bee
gokayari <gokayari@gmail.com>
2024-08-17 17:46:14 +0200

    first commit: Hello World

---
+++ 1_hello_world.py
@@ -0,0 +1,20 @@
+print("Hello World")    # this is print statement
+print('Hallo Welt')     # this is also print statement
+
+"""
+multiline comment
+"""
+
+
+print('Our\nSchool')
+print('\t\tPython Programming')
+print('\\')
+
+print('')
+
+print('I love \"Python\"')
+print('I love "Python"')
+
+# print("I love "Python"")  >>> SyntaxError: invalid syntax
+# print('I love 'Python'')  >>> SyntaxError: invalid syntax
+
