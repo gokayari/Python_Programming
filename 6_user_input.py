25d57bb143f5c879a1aec94852b1d03c7577bad0
gokayari <gokayari@gmail.com>
2024-08-20 09:52:08 +0200

    user input

---
+++ 6_user_input.py
@@ -0,0 +1,18 @@
+
+# print(help(input))        # >>> documentation of the method
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+# nation = input('Enter your nationality: ')
+gpa = float(input('Enter your gpa: '))
+
+# print(name,'is',age,'years old and from',nation)
+
+
+print(type(name))
+print(type(age))
+print('----')
+
+print(age)
+print(int(age) - 10)
+print(type(gpa))
